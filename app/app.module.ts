2224d1973c61193deb26978a81c6bebb24ab1b2b
Jordan Ramey <jramey@qualtrax.com>
2016-09-15 20:31:58 -0400

    Show our Hero

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,10 @@
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
 import { NgModule }      from '@angular/core';
 
 @NgModule({
-  imports:      [ BrowserModule],
+  imports:      [ BrowserModule, FormsModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
